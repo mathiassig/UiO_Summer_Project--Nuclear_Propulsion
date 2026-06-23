f7c5ea3a66a165a93c7fe0a898b36da0a959e701
mathiassig <mathisi@stud.ntnu.no>
2026-06-23 11:49:43 +0200

    added __str__ function for Ship class

--- ship.py
+++ ship.py
@@ -7,5 +7,13 @@ class Ship:
         self.op_time = op_time
         self.portcalls = portcalls
         self.name = name
-    
+    def __str__(self):
+        return (
+            f"=== {self.name} ship characteristics ===\n"
+            f"Installed propulsion power: {self.propulsion} MW\n"
+            f"Installed auxiliary power:  {self.auxiliary} MW\n"
+            f"Average speed:  {self.avg_speed} knots\n"
+            f"Operational time:    {self.op_time} hours\n"
+            f"Number of portcalls per year:  {self.portcalls}"
+        )    
     
